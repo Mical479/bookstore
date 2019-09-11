6355cc99735516420d3012631dd06f1eff78081c
太阳 <940607348@qq.com>
2019-09-11 20:39:06 +0800

    bookAppraiseService

--- src/main/java/com/bookstore/service/impl/BookAppraiseServicempl.java
+++ src/main/java/com/bookstore/service/impl/BookAppraiseServicempl.java
@@ -3,6 +3,8 @@ package com.bookstore.service.impl;
 import com.bookstore.mapper.BookAppraiseMapper;
 import com.bookstore.pojo.BookAppraise;
 import com.bookstore.service.BookAppraiseService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
 
 import java.util.List;
 
@@ -12,6 +14,7 @@ import java.util.List;
  * 创建时间：2019/9/11 15:17
  * 创建人：slx
  */
+@Service
 public class BookAppraiseServicempl implements BookAppraiseService {
     private BookAppraiseMapper bookAppraiseMapper;
 
@@ -20,6 +23,7 @@ public class BookAppraiseServicempl implements BookAppraiseService {
      * @param book_id  书籍id
      * @return
      */
+    @Autowired
     public List<BookAppraise> getAllAppraise(int book_id){
         return bookAppraiseMapper.selectAllAppraisesById(book_id);
     }
@@ -29,6 +33,7 @@ public class BookAppraiseServicempl implements BookAppraiseService {
      * @param book_id  书籍id
      * @return
      */
+    @Autowired
     public List<Integer> getNumOfReplyAppraises(int book_id){
         return bookAppraiseMapper.countReplayAppraises(book_id);
     }
@@ -38,6 +43,7 @@ public class BookAppraiseServicempl implements BookAppraiseService {
      * @param book_id  书籍id
      * @return
      */
+    @Autowired
     public int getNumOfAppraises(int book_id){
         return bookAppraiseMapper.countApprases(book_id);
     }
